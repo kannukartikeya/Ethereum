10f25f3ad374379ef95165c9e027004e8e76b472
kartikeya <kartikeya_g@rediffmail.com>
2017-12-25 20:28:58 +0530

    Voting UI

--- solidity-experiments/migrations/3_deploy_Voting.js
+++ solidity-experiments/migrations/3_deploy_Voting.js
@@ -2,5 +2,5 @@ var votingContract = artifacts.require("./MyVoting.sol");
 
 module.exports = function(deployer) {
   // Pass 42 to the contract as the first constructor parameter
-  deployer.deploy(votingContract, ['Kartik','Ram'])
+  deployer.deploy(votingContract, ['Kartik','Rama','Jose'])
 };
